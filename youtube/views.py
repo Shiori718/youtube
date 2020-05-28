7995f8b54ebba74b3f37eeef12df55069095b305
Shiori718 <shiori.masaki777@gmail.com>
2020-05-28 18:25:25 +0900

    top page

--- youtube/views.py
+++ youtube/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     # return HttpResponse('Hello World from Youtube')
     return render(request, 'youtube/index.html')
+
+def contact(request):
+    # return HttpResponse('Hello World from Youtube')
+    return render(request, 'youtube/contact.html')
